f5f81d651f5929df081317c48f6f23a39504c232
K1ngJur0 <79678174+K1ngJur0@users.noreply.github.com>
2021-02-26 10:26:27 -0500

    Update window.py

--- window.py
+++ window.py
@@ -1,11 +1,16 @@
 import pygame
+import object as obj
+
+pygame.init()
 
 def createWindow():
     screenSize = (800, 600)
-    backgroundColor = (0, 0, 255)
+    backgroundColor = (255, 255, 255)
 
     screen = pygame.display.set_mode(screenSize)
     pygame.display.set_caption("The Most Amazing Window Ever Made")
     screen.fill(backgroundColor)
+
+    pygame.Surface.blit(screen, obj.player, (obj.xPos, obj.yPos))
 
     pygame.display.flip()
