9e219875eaa3af4873de3b6c04232dcbc08cd168
K1ngJur0 <79678174+K1ngJur0@users.noreply.github.com>
2021-03-04 21:05:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,4 @@ pygame.init()
 
 wnd.createWindow()
 
-running = True
-
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        pygame.display.update()
+wnd.gameLoop()
